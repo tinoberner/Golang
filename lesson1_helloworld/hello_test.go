93e1dd2ab6557ddb1a3d65084a1fbcfff76b7578
Tino <test@tinoberner.com>
2024-11-07 10:32:25 +0100

    adding spanish and french test

--- lesson1_helloworld/hello_test.go
+++ lesson1_helloworld/hello_test.go
@@ -3,9 +3,34 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean-Luc", "French")
+		want := "Bonjour, Jean-Luc"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() will point to the failed test not the origin of the message
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
